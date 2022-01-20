4ad174f24619e55c6cec9a34ac1696f4e8019f44
Shawna Hightower <sdhightower6@gmail.com>
2022-01-20 16:16:44 -0700

    update

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/product-manager", {
-
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect("mongodb://localhost/product-manager")
 
     .then(() => console.log('Established a connection to the database'))
     .catch(error => console.log('Something went wrong when connecting to the database', error));
